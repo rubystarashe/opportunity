81d5d1ec5ba5847c0eecf67f4e9a6fcd4ad72131
RubystarAshe <rubystarashe@gmail.com>
2018-04-18 22:37:23 +0900

    [M/NUXT] generate router 수정

--- .nuxt/utils.js
+++ .nuxt/utils.js
@@ -119,7 +119,7 @@ export async function setContext(app, context) {
       
       payload: context.payload,
       error: context.error,
-      base: '/',
+      base: '/opportunity/',
       env: {}
     }
     // Only set once
