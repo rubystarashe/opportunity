a25e606c40e75d023974c3947143a3db2bc12022
RubystarAshe <rubystarashe@gmail.com>
2018-04-18 22:39:44 +0900

    [M/PAGE] 예제용 SpeechRecognition API

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,24 @@ export default {
       interm: '',
 			transcript: []
 		}
+	},
+	mounted() {
+    const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+		const recognition = new SpeechRecognition()
+		recognition.continuous = true
+		recognition.interimResults = true
+		recognition.lang = 'ko-KR'
+		recognition.start()
+    const self = this
+		recognition.onresult = function(event) {
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          self.transcript.push(event.results[i][0].transcript)
+        } else {
+          self.interm = event.results[i][0].transcript
+        }
+      }
+		}
 	}
 }
 </script>
